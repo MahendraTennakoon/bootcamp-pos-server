671f66c8a99d1e427694d7b1481cce57efa82521
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-09 10:45:51 +0530

    Setup server startup.

--- app/server.js
+++ app/server.js
@@ -1,3 +1,15 @@
-import express from 'express';
+const express = require('express');
+// For parsing incoming request bodies.
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Allow rich content (arrays/objects) in url encoded data.
+app.use(bodyParser.urlencoded({ extended: true }));
+// Parse JSON data in request bodies.
+app.use(bodyParser.json());
+
+// Start server -------------------------------------------------
+const port = 8080;
+app.listen(8080);
+console.log(`pos-server listening on port ${port}.`);
