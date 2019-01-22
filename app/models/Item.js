e3f87bcab0022d3e573be46b78e59d536453486c
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-22 10:53:56 +0530

    Separate config file for sequelize

--- app/models/Item.js
+++ app/models/Item.js
@@ -1,19 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('bootcamp_pos', 'john_doe', '1qaz2wsx', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorsAliases: false,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    define: {
-        freezeTableName: true,
-        timestamps: false
-    }
-});
+const sequelize = require('../../config/sequelize');
 
 const Item = sequelize.define('item', {
     id: {
