ec69ca99612b01b98cb342e78a94d29b37f4ef48
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-23 15:36:59 +0530

    Use bodyParser in index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,12 @@
 const app = require('express')();
 
+// For parsing incoming request bodies.
+const bodyParser = require('body-parser');
+// Parse JSON data in request bodies.
+app.use(bodyParser.json());
+// Allow rich content (arrays/objects) in url encoded data.
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/items', require('./items'));
 app.use('/users', require('./users'));
 app.use('/orders', require('./orders'))
--- tests/orders.test.js
+++ tests/orders.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app/server');
+const app = require('../app/routes/index');
 
 /**
  * Delete all orders in orders table.
