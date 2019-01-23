6e3fd4a75ff520dc2082ea7911cbce9536f2810a
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-23 10:23:28 +0530

    Fix test case for orders/:order_id route

--- tests/orders.test.js
+++ tests/orders.test.js
@@ -20,18 +20,16 @@ test('orders route', async () => {
 test('orders/:order_id route', async () => {
     const order = [
         {
-            id: 1,
-            created_date: "2019-01-22",
-            name: "Classic Burger",
-            price: 450,
-            quantity: 12
+            "id": 1,
+            "name": "Classic Burger",
+            "price": 450,
+            "quantity": 12
         },
         {
-            id: 2,
-            created_date: "2019-01-22",
-            name: "Backpackers Burger",
-            price: 725,
-            quantity: 21
+            "id": 2,
+            "name": "Backpackers Burger",
+            "price": 725,
+            "quantity": 21
         }
     ];
 
