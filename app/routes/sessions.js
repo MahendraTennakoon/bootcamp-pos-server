4d0b50a3cf6d1622a9770e0f05c41264c39c4a64
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-01-30 10:04:41 +0530

    Minial login functionality

--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -6,9 +6,13 @@ const User = require('../models/User')
 router.route('/')
     .post((req, res, next) => {
         User.findOne({ where: { user_name: req.body.user_name } }).then(user => {
-            if (user) {
+            if (user && user.password === req.body.password) {
                 res.cookie('pos_app_session', user.user_id);
-                res.json(user);
+                res.json({
+                    user_name: user.user_name,
+                    first_name: user.first_name,
+                    last_name: user.last_name
+                });
             } else {
                 res.send(401);
             }
