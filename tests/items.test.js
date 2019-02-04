16611432c023042d504f10821602481d622940b7
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-02-04 20:35:59 +0530

    Tests for items route.

--- tests/items.test.js
+++ tests/items.test.js
@@ -1,34 +1,30 @@
 const request = require('supertest');
 const app = require('../app/routes/index');
+const items = require('./migrations/items.json');
+const Item = require('../app/models/Item');
+
+/**
+ * Delete all orders in orders table.
+ */
+const clearItems = () => {
+    return Item.destroy({
+        where: {}
+    })
+};
+
+/**
+ * Populate orders table.
+ */
+const populateItems = () => {
+    return Item.bulkCreate(items)
+};
+
+beforeEach(() => {
+    return clearItems()
+        .then(() => populateItems())
+});
 
 test('items route', async () => {
-    const items = [
-        {
-            "id": 1,
-            "name": "Classic Burger",
-            "price": 450
-        },
-        {
-            "id": 2,
-            "name": "Backpackers Burger",
-            "price": 725
-        },
-        {
-            "id": 3,
-            "name": "Bistro Burger",
-            "price": 250
-        },
-        {
-            "id": 4,
-            "name": "Chicken Burger",
-            "price": 300
-        },
-        {
-            "id": 5,
-            "name": "BBQ Burger",
-            "price": 360
-        }
-    ];
     const response = await request(app).get('/items');
     expect(response).toBeDefined();
     expect(response.body).toEqual(items);
