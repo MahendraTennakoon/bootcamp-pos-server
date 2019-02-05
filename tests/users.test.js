fe7c25c9bcafbf2f3a634315b87a52e18ba8763f
Mahendra Tennakoon <mahendra.tennakoon@syscolabs.com>
2019-02-05 18:40:20 +0530

    fix tests for users route.

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,18 +1,42 @@
 const request = require('supertest');
 const app = require('../app/routes/index');
+const User = require('../app/models/User');
+const users = require('../tests/migrations/users.json');
+
+const clearUsers = () => {
+    return User.destroy({
+        where: {},
+        truncate: true
+    });
+};
+
+const populateUsers = () => {
+    return User.bulkCreate(users);
+};
+
+beforeAll(() => {
+    return clearUsers()
+        .then(() => populateUsers())
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+afterAll(() => {
+    return clearUsers();
+});
 
 test('users route', async () => {
-    const users = [
-        {
-            "user_id": 1,
-            "first_name": "john",
-            "last_name": "doe",
-            "user_name": "johndoe",
-            "password": "123"
+    let response = await request(app).get('/users');
+    response = response.body.map((user) => {
+        return {
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "password": user.password,
+            "user_name": user.user_name
         }
-    ];
-    const response = await request(app).get('/users');
+    });
     expect(response).toBeDefined();
-    expect(response.body).toEqual(users);
+    expect(response).toEqual(users);
 });
 
